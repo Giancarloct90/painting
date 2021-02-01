d9cb8029d1926d827d01ad9b8049ff8af7a8b1c1
Giancarlo <giancarloct90@hotmail.com>
2021-01-31 22:07:08 -0600

    contact us

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-console.log('hello wolrd');
+const navlinks = document.getElementById('navlinks');
+const burger = document.getElementById('burger');
+const navLinksActive = document.querySelectorAll('.navlinks li');
+
+burger.addEventListener('click', ()=>{
+    //toggle class
+    navlinks.classList.toggle('navLinksActive');
+    
+    //Animation li 
+    navLinksActive.forEach((link,index)=>{
+        if(link.style.animation){
+            link.style.animation = '';
+        }else{
+            link.style.animation= `navLinksAnimations 0.5s ease forwards ${index/7+0.3}s`
+        }
+    });
+
+    //Convert tha humburger to X
+    burger.classList.toggle('toggle');
+});
+
