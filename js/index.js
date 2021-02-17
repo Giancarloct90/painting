9756b512fda10959b3c84d84dff58840e26bb7d2
giancarloct90 <giancarloct90@hotmail.com>
2021-02-17 15:59:04 -0600

    final 3

--- js/index.js
+++ js/index.js
@@ -3,6 +3,12 @@ const burger = document.getElementById('burger');
 const navLinksActives = document.querySelectorAll('.navlinks li');
 const li = document.getElementsByTagName('li');
 
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 burger.addEventListener('click', ()=>{
     navlinks.style.display = ''
     //toggle class
