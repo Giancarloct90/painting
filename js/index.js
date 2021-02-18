b83abc1c35a5ed7339ec27bc7b9902a22915fbd7
Giancarlo <giancarloct90@hotmail.com>
2021-02-17 20:34:19 -0600

    final 4

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+// We execute the same script as before
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const navlinks = document.getElementById('navlinks');
 const burger = document.getElementById('burger');
 const navLinksActives = document.querySelectorAll('.navlinks li');
