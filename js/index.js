e0d9f65ca99c8dddfb286cefdb970dd87a628c11
Giancarlo <giancarloct90@hotmail.com>
2021-02-22 17:58:53 -0600

    final 5

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 // We execute the same script as before
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// window.addEventListener('resize', () => {
+//     // We execute the same script as before
+//     let vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+//   });
 
 const navlinks = document.getElementById('navlinks');
 const burger = document.getElementById('burger');
 const navLinksActives = document.querySelectorAll('.navlinks li');
 const li = document.getElementsByTagName('li');
-
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
 
 burger.addEventListener('click', ()=>{
     navlinks.style.display = ''
