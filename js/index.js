a6b0246d7a3cc2592a7bfe8db9bd7cea8239e2ab
Giancarlo <giancarloct90@hotmail.com>
2021-02-22 20:48:33 -0600

    final 7

--- js/index.js
+++ js/index.js
@@ -55,6 +55,16 @@ navLinksActives.forEach(item =>{
 });
 
 window.addEventListener('scroll', ()=>{
-    navlinks.style.display = 'none'
-})
+
+    if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)){
+            navlinks.style.display = 'none';
+    }
+ 
+});
 
